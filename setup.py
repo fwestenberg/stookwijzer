cd6fbe92bbbe436101d68c547411a0c5f32cb989
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2022-12-23 10:10:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'stookwijzer',
   packages = ['stookwijzer'],
-  version = '1.0.0',
+  version = '1.1.0',
   license='MIT',
   description = 'Stookwijzer package',
   author = 'fwestenberg',
