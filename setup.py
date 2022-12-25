ed178497fd0a82e286077f3052d28999145c5f36
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2022-12-25 09:55:08 +0100

    Update setup.py
    - Rounding of the lqi value

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'stookwijzer',
   packages = ['stookwijzer'],
-  version = '1.1.0',
+  version = '1.2.0',
   license='MIT',
   description = 'Stookwijzer package',
   author = 'fwestenberg',
