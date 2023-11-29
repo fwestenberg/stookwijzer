427843c1273e48c71d81ce2b0c5ee39c030a792a
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2023-11-30 00:09:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   license='MIT',
   description = 'Stookwijzer package',
   long_description_content_type="text/markdown",
-  long_description=file: README.md,
+  long_description='Stookwijzer package',
   author = 'fwestenberg',
   author_email = '',
   url = 'https://github.com/fwestenberg/stookwijzer',
